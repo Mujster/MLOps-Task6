a81286da63659d3251ef345250843f0cc8481b74
mujtaba ahmad <mmujtabach1234@gmail.com>
2024-09-30 14:06:30 +0500

    Final Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import os
+import psycopg2
+
+app = Flask(__name__)
+
+# Example route
+@app.route('/')
+def home():
+    return "Hello from Flask with Docker Compose!"
+
+# Connecting to the PostgreSQL database
+def get_db_connection():
+    conn = psycopg2.connect(os.getenv('DATABASE_URL'))
+    return conn
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
